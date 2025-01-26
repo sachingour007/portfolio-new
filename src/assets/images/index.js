7b2cd466fc59af9c028e29ae65583c6a899624c6
sachingour <gour.sachin.sg@gmail.com>
2025-01-26 20:54:59 +0530

    my portfolio black theme code

---
+++ src/assets/images/index.js
@@ -0,0 +1,2 @@
+export { default as linkedinIcon } from "./linkedin-icon.svg";
+export { default as githubIcon } from "./github-icon.svg";
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <>
+      <section className="bannerSec">
+        <div className="wrapperSec">
+          <div className="bannerHeading">
+            <h1>Sachin Gour</h1>
+            <p>Web-developer</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Homepage;
