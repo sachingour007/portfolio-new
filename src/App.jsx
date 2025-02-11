3567c156c43fadadb50aac80f70ba01605340277
sachingour <gour.sachin.sg@gmail.com>
2025-02-11 23:50:26 +0530

    project layout changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import react from "react";
-import Homepage from "./pages/Homepage";
-import Header from "./pages/Header";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Homepage />
+     
     </BrowserRouter>
   );
 }
--- src/assets/images/index.js
+++
@@ -1,7 +0,0 @@
-export { default as linkedinIcon } from "./linkedin-icon.svg";
-export { default as githubIcon } from "./github-icon.svg";
-export { default as htmlIcon } from "./HTML5.svg";
-export { default as cssIcon } from "./CSS3.svg";
-export { default as javascriptIcon } from "./Javascript.svg";
-export { default as reactIcon } from "./React.svg";
-export { default as nodeIcon } from "./node.svg";
