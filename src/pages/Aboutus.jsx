78040e706889aa662d75e06d406c6f9636d557ac
sachingour <gour.sachin.sg@gmail.com>
2025-03-01 23:06:02 +0530

    about me code

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Aboutus = () => {
   return (
-    <div>Aboutus</div>
-  )
-}
+    <div className="aboutUsWrapper">
+      <h2>About <span className="orange-text">Me</span></h2>
+      <h1>
+        I'm someone who finds profound joy in the art of design and <span className="orange-text">coding</span>
+      </h1>
+      <p>
+        With an unwavering passion for crafting beautiful and simple creations,
+        I pour my heart into every project I undertake. My work is a reflection
+        of my dedication to merging aesthetics with user-centered functionality.
+        With each design and code, I aim to create solutions that not only look
+        pleasing but also make life easier for users. Welcome to my world of
+        elegant simplicity, where every line of code and every pixel hold the
+        essence of my love for what I do.
+      </p>
+    </div>
+  );
+};
 
-export default Aboutus
+export default Aboutus;
