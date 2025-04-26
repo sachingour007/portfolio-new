fdddfc6ba2d8abc73266abbba5c5ab483db88c04
sachingour <gour.sachin.sg@gmail.com>
2025-04-27 00:14:10 +0530

    Contact us page

--- src/components/HeadingComponent.jsx
+++ src/components/HeadingComponent.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const HeadingComponent = ({ normalText, orangeText }) => {
   return (
     <>
-      <h2>
+      <h1 className="customHeading">
         {normalText} <span className="orange-text">{orangeText}</span>
-      </h2>
+      </h1>
     </>
   );
 };
--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import HeadingComponent from "../components/HeadingComponent";
 
 const ContactMe = () => {
+  const [formVal, setFormVal] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const valHandler = (e) => {
+    setFormVal({ ...formVal, [e.target.name]: e.target.value });
+  };
+  const formSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="contactUsWrapper">
       <HeadingComponent normalText="Contact" orangeText="Me" />
-      <h1>Let's get in touch!</h1>
+      <h2>Let's get in touch!</h2>
+      <div className="formBox">
+        <form onSubmit={formSubmit}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your Name"
+            onChange={valHandler}
+          />
+          <input
+            type="text"
+            placeholder="Enter your Email"
+            name="email"
+            onChange={valHandler}
+          />
+          <textarea
+            id=""
+            placeholder="Enter your massage"
+            name="message"
+            onChange={valHandler}
+          ></textarea>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
