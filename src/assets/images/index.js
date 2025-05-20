fe59017ce9e73d29f78140e5a2dbd92cbfa8ae52
sachingour <gour.sachin.sg@gmail.com>
2025-05-20 13:40:33 +0530

    index file update

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,8 +1,3 @@
-export { default as facebook } from "./facebook.svg";
-export { default as twitter } from "./twitter.svg";
-export { default as insta } from "./instagram.svg";
-export { default as linkedin } from "./linkedin.svg";
-export { default as google } from "./google.svg";
 export { default as profilePic } from "./profilePic.webp";
 export { default as homepage } from "./home.svg";
 export { default as htmlIcon } from "./html-icon.svg";
